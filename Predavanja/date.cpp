d23a3f79303da8107fec3b841549706591eaa3bb
Muhamed Karajic <35216938+muhamedkarajic@users.noreply.github.com>
2018-10-12 09:51:00 +0200

    Added date and datum!

---
+++ Predavanja/date.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+using namespace std;
+
+class Date
+{
+	int *_day;
+	int *_month;
+	int *_year;
+
+public:
+
+	void setDay(const int &day) { *_day = day; }
+	void setMonth(const int &month) { *_month = month; }
+	void setYear(const int &year) { *_year = year; }
+
+	const char* getDateChar() const
+	{
+		if (_day == nullptr || _month == nullptr || _year == nullptr)
+			return nullptr;
+
+		char day[3], month[3], year[5];
+
+		_itoa_s(*_day, day, 10);
+		_itoa_s(*_month, month, 10);
+		_itoa_s(*_year, year, 10);
+
+		int len = 3;//date 11.10.2018
+
+		len += strlen(day);
+		len += strlen(month);
+		len += strlen(year);
+
+
+		char *temp = new char[len];
+		strcpy_s(temp, len, day);
+		strcat_s(temp, len, ".");
+		strcat_s(temp, len, month);
+		strcat_s(temp, len, ".");
+		strcat_s(temp, len, year);
+
+		return temp;
+	}
+
+	Date() { _day = _month = _year = nullptr; }
+	
+	Date(const int &day, const int &month, const int &year) {
+		_day = new int(day);
+		_month = new int(month);
+		_year = new int(year);
+	}
+	
+	bool leapYear() const { return *_year % 4 == 0 || *_year % 100 == 0 || *_year % 400 == 0; }
+
+	int MonthDays(const int &month) const
+	{
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+			return 31;
+		else if (month == 2)
+			return 28 + leapYear();
+		return 30;
+	}
+
+	bool ValiDate(const int &day, const int &month, const int &year) 
+		{ return day >= 1 && day <= MonthDays(month) && month >= 1 && month <= 12 && year >= 2000; }
+
+	Date(const Date &date)
+	{
+		_day = new int(*date._day);
+		_month = new int(*date._month);
+		_year = new int(*date._year);
+	}
+
+	void operator=(const Date &date)
+	{
+		_day = new int(*date._day);
+		_month = new int(*date._month);
+		_year = new int(*date._year);
+	}
+
+	Date AddDays(int day)//11.10.2018
+	{
+		Date newDate = *this;//operator=
+
+		if (ValiDate(*newDate._day + day, *newDate._month, *newDate._year))
+		{
+			*newDate._day += day;
+			return newDate;
+		}
+
+		int daysLeft;
+		while (day > 0)
+		{
+			daysLeft = MonthDays(*newDate._month) - *newDate._day + 1;
+			if (day >= daysLeft) {
+				day -= daysLeft;
+				(*newDate._month)++;
+				*newDate._day = 1;
+				if (*newDate._month > 12)
+				{
+					*newDate._month -= 12;
+					(*newDate._year)++;
+				}
+			}
+			else
+			{
+				(*newDate._day) += day;
+				day = 0;
+			}
+		}
+
+		(*newDate._day) += day;
+		return newDate;
+	}
+};
+
+int main()
+{
+	int day;
+	Date date(11, 10, 2018);
+	do
+	{
+		cin >> day;
+		
+		date.setDay(11);
+		date.setMonth(10);
+		date.setYear(2018);
+
+		cout << date.AddDays(day).getDateChar() << endl;
+	} while (day != 0);
+	
+	system("PAUSE>0");
+}
---
+++ Predavanja/datum.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+using namespace std;
+
+class Datum
+{
+	int *_dan;
+	int *_mjesec;
+	int *_godina;
+
+public:
+
+	void setDan(const int &dan) { *_dan = dan; }
+	void setMjesec(const int &mjesec) { *_mjesec = mjesec; }
+	void setGodinu(const int &godina) { *_godina = godina; }
+
+	const char* getDatumChar() const
+	{
+		if (_dan == nullptr || _mjesec == nullptr || _godina == nullptr)
+			return nullptr;
+
+		char dan[3], mjesec[3], godina[5];
+
+		_itoa_s(*_dan, dan, 10);
+		_itoa_s(*_mjesec, mjesec, 10);
+		_itoa_s(*_godina, godina, 10);
+
+		int vel = 3;//datum 11.10.2018
+
+		vel += strlen(dan);
+		vel += strlen(mjesec);
+		vel += strlen(godina);
+
+
+		char *temp = new char[vel];
+		strcpy_s(temp, vel, dan);
+		strcat_s(temp, vel, ".");
+		strcat_s(temp, vel, mjesec);
+		strcat_s(temp, vel, ".");
+		strcat_s(temp, vel, godina);
+
+		return temp;
+	}
+
+	Datum() { _dan = _mjesec = _godina = nullptr; }
+	
+	Datum(const int &dan, const int &mjesec, const int &godina) {
+		_dan = new int(dan);
+		_mjesec = new int(mjesec);
+		_godina = new int(godina);
+	}
+	
+	bool PrestupnaGodina() const { return *_godina % 4 == 0 || *_godina % 100 == 0 || *_godina % 400 == 0; }
+
+	int DaniMjeseca(const int &mjesec) const
+	{
+		if (mjesec == 1 || mjesec == 3 || mjesec == 5 || mjesec == 7 || mjesec == 8 || mjesec == 10 || mjesec == 12)
+			return 31;
+		else if (mjesec == 2)
+			return 28 + PrestupnaGodina();
+		return 30;
+	}
+
+	bool ValidanDatum(const int &dan, const int &mjesec, const int &godina) 
+		{ return dan >= 1 && dan <= DaniMjeseca(mjesec) && mjesec >= 1 && mjesec <= 12 && godina >= 2000; }
+
+	Datum(const Datum &datum)
+	{
+		_dan = new int(*datum._dan);
+		_mjesec = new int(*datum._mjesec);
+		_godina = new int(*datum._godina);
+	}
+
+	void operator=(const Datum &datum)
+	{
+		_dan = new int(*datum._dan);
+		_mjesec = new int(*datum._mjesec);
+		_godina = new int(*datum._godina);
+	}
+
+	Datum DodajDane(int dani)//11.10.2018
+	{
+		Datum noviDatum = *this;//operator=
+
+		if (ValidanDatum(*noviDatum._dan + dani, *noviDatum._mjesec, *noviDatum._godina))
+		{
+			*noviDatum._dan += dani;
+			return noviDatum;
+		}
+
+		int razlika;
+		while (dani > 0)
+		{
+			razlika = DaniMjeseca(*noviDatum._mjesec) - *noviDatum._dan + 1;
+			if (dani >= razlika) {
+				dani -= razlika;
+				(*noviDatum._mjesec)++;
+				if (*noviDatum._mjesec > 12)
+				{
+					*noviDatum._mjesec -= 12;
+					(*noviDatum._godina)++;
+				}
+				*noviDatum._dan = 1;
+			}
+			else
+			{
+				(*noviDatum._dan) += dani;
+				dani = 0;
+			}
+		}
+
+		(*noviDatum._dan) += dani;
+		return noviDatum;
+	}
+};
+
+int main()
+{
+	int dani;
+	Datum datum(11, 10, 2018);
+	do
+	{
+		cin >> dani;
+		
+		datum.setDan(11);
+		datum.setMjesec(10);
+		datum.setGodinu(2018);
+
+		cout << datum.DodajDane(dani).getDatumChar() << endl;
+	} while (dani != 0);
+	
+	system("PAUSE>0");
+}
